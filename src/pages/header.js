2a4e705f92fd34489fdbee89e6f7a23be9587a43
nora2737 <norah2737@gmail.com>
2021-01-18 10:53:05 +0300

    7

--- src/pages/header.js
+++ src/pages/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom"
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined';
+//import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined';
 const Header = () => {
 
   return (
@@ -12,7 +12,8 @@ const Header = () => {
           <Link className="nav-item mx-3" to="/">Home</Link>&nbsp;
           <Link className="nav-item mx-3" to="/products">Prints</Link>&nbsp;
           <Link className="nav-item mx-3" to="/Artists">Artists</Link>&nbsp;
-          <Link className="cart-icon mx-3" to="/Cart"> <ShoppingBasketOutlinedIcon></ShoppingBasketOutlinedIcon></Link>&nbsp;
+          <Link className="cart-icon mx-3" to="/Cart">
+            Cart</Link>&nbsp;
         </Nav>
       </Navbar>
     </>
